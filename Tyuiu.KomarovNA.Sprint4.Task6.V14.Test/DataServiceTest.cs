4d699962677d5faa0b3d3519ef9dabb5d182ed7e
gravity34031 <gravity2507@gmail.com>
2024-09-26 23:18:25 +0500

    Task6

---
+++ Tyuiu.KomarovNA.Sprint4.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KomarovNA.Sprint4.Task6.V14.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint4.Task6.V14/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KomarovNA.Sprint4.Task6.V14.Lib;
+namespace Tyuiu.KomarovNA.Sprint4.Task6.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль"]);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
