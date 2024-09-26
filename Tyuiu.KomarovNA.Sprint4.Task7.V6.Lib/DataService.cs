c1d56fe09165b3a2360433046448b3d4c0b094ae
gravity34031 <gravity2507@gmail.com>
2024-09-26 23:26:27 +0500

    Task7

---
+++ Tyuiu.KomarovNA.Sprint4.Task7.V6.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KomarovNA.Sprint4.Task7.V6.Lib
+{
+    public class DataService : ISprint4Task7V6
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] matrix = new int[n, m];
+            int indx = 0;
+            for (int i = 0; i<matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j<matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = int.Parse(value[indx].ToString());
+                    indx++;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i, j]%2 != 0)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+
+
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint4.Task7.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KomarovNA.Sprint4.Task7.V6.Lib;
+namespace Tyuiu.KomarovNA.Sprint4.Task7.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #6                                                            *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(3, 4, "458963214789");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
